bb2ee0fe520ec7f60bf0b3fc193df91c1631e36f
PGL3D <philip.larson@gmail.com>
2026-05-23 19:37:28 +0000

    Initial app setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-5xl font-bold mb-4">
+        Bay Area Shows
+      </h1>
+
+      <p className="text-xl text-gray-300">
+        Discover upcoming concerts with GA tickets
+      </p>
+    </main>
+  );
+}
